9dd1e814c4e053a50cb574574b91acfc03bfcec8
Ben Bianchi <bgbianchi@wpi.edu>
2018-07-15 08:37:32 -0400

    Moving computers

--- src/main/java/com/bianchi/ch1/ArraysAndStrings.java
+++ src/main/java/com/bianchi/ch1/ArraysAndStrings.java
@@ -1,8 +1,9 @@
 package com.bianchi.ch1;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 
-import javax.swing.JTextArea;
+import javax.swing.text.html.HTMLDocument.HTMLReader.FormAction;
 
 /**
  * The following code are my attempts at the answer of Cracking the Code Interview's questions.
@@ -72,6 +73,12 @@ import javax.swing.JTextArea;
     }
 
     /**
+     * Question 2
+     * 
+     * Check to see if a sting is a palindrome.
+     */
+
+    /**
      * Checks to see if a string is a permutation of another string
      * 
      * @param original The Base string
@@ -104,5 +111,119 @@ import javax.swing.JTextArea;
         }
 
         return true;        
+    }
+
+    /**
+     * Question 3:
+     * 
+     * Write a method toWrite a method to replace all the spaces in a string with ‘%20’.
+     * You may assume that the string has sufficient space at the end to hold the additional characters, 
+     * and that you are given the “true” length of the string.
+     */
+
+     /**
+      * 
+      * @param stringToBecomeURL the string we are converting to a url
+      * @param trueLengthOfString an integer that is the true length of the 
+      *
+      * @return String a string that is the url representation of the URL.
+      */
+    public static String convertToURL(String stringToBecomeURL, Integer trueLengthOfString) {
+        
+        if (stringToBecomeURL == null || trueLengthOfString == null) {
+            return null;
+        }
+
+        char[] urlCharacters = stringToBecomeURL.toCharArray();
+        StringBuilder sBuilder = new StringBuilder();
+
+        for (int i = 0; i < trueLengthOfString; i++) {
+            if (urlCharacters[i] == ' ') {
+                sBuilder.append("%20");
+            } else {
+                sBuilder.append(urlCharacters[i]);
+            }
+        }
+        return sBuilder.toString();
+    }
+
+    /**
+     * Question 1.4
+     * Given a string, check to see if it is a permutation of a palindrome.
+     */
+
+    /**
+     * Checks to see if a string is a permutation of a palindrome.
+     * 
+     * Under the covers, this looks to see if the occurances of each character is divisible
+     * by two, and only allows one character to have one occurance. This singular character can be the
+     * midpoint.
+     * 
+     * @param permutation The string we are checking to see is a permutation of a Palindrome
+     * 
+     * @return Boolean whether or not this string is a permutation of a palindrome.
+     */
+    public static Boolean isPalindromePermutation(String permutation) {
+        if (permutation  == null) {
+            return false;
+        }
+
+        HashMap<Character, Integer> characterOccuranceMap = new HashMap<>();
+
+        for (char character : permutation.toLowerCase().toCharArray()) {
+            if (character == ' ') {
+                continue;
+            }
+
+            if (characterOccuranceMap.containsKey(character)) {
+                Integer previousValue = characterOccuranceMap.get(character);
+                characterOccuranceMap.put(character, ++previousValue);
+            } else {
+                characterOccuranceMap.put(character, 1);
+            }
+        }
+        
+        if (permutation.length() % 2 == 0) {
+            Boolean midpointFound = false;
+            for (Entry<Character, Integer> characterOccuranceCountEntry : characterOccuranceMap.entrySet()) {
+                if (characterOccuranceCountEntry.getValue() % 2 == 1) {
+                    if (midpointFound) {
+                        return false;
+                    } else {
+                        midpointFound = true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static Boolean isOneEditAway(String inputOne, String inputTwo) {
+        if (inputOne == null || inputTwo == null) {
+            return null;
+        }
+
+        if (inputOne.equals(inputTwo)) {
+            return true;
+        }
+
+        if (Math.abs(inputOne.length() - inputTwo.length() > 1)) {
+            return false;
+        }
+
+        // After input sanitization and sanity check there are 3 cases here:
+        // 1. inputOne is too short -- try inserting
+        // 2. inputOne is too long -- try deleting
+        // 3. inputOne is the same size -- try changing
+
+        if (inputOne.length() < inputTwo.length()) {
+
+        }
+    }
+
+    private static Boolean attemptInsertion(String inputOne, String inputTwo) {
+        for (int i = 0; i < inputOne.length(); i++) {
+            if (inputOne.charAt(i) != inputTwo.charAt(i))   
+        }
     }
  }
--- src/test/java/com/bianchi/ch1/ArraysAndStringsTest.java
+++ src/test/java/com/bianchi/ch1/ArraysAndStringsTest.java
@@ -1,74 +1,182 @@
 package com.bianchi.ch1;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class ArraysAndStringsTest extends TestCase {
+import static org.junit.Assert.*;
+
+import static com.bianchi.ch1.ArraysAndStrings.*;
+
+public class ArraysAndStringsTest {
 
     /**
      * Question 1
      * 
-     * Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+     * Implement an algorithm to determine if a string has all unique characters.
+     * What if you cannot use additional data structures?
      */
 
-     public void testNullShouldShortCircuitToFalse () {
-         assertFalse(ArraysAndStrings.isUnique(null));
-     }
+    @Test
+    public void testNullShouldShortCircuitToFalse() {
+        assertFalse(isUnique(null));
+    }
 
-     public void testEmptyShouldShortCircuitToFalse () {
-         assertFalse(ArraysAndStrings.isUnique(""));
-     }
+    @Test
+    public void testEmptyShouldShortCircuitToFalse() {
+        assertFalse(isUnique(""));
+    }
 
-     public void testStringWithDuplicatesIsNotUnique() {
-         assertFalse(ArraysAndStrings.isUnique("object oriented"));
-     }
+    @Test
+    public void testStringWithDuplicatesIsNotUnique() {
+        assertFalse(isUnique("object oriented"));
+    }
 
-     public void testStringWithoutDuplicatesIsUnique() {
-         assertTrue(ArraysAndStrings.isUnique("abc"));
-     }
+    @Test
+    public void testStringWithoutDuplicatesIsUnique() {
+        assertTrue(isUnique("abc"));
+    }
 
-     /**
-      * Question 1 w/o hashmap gives us a O(N^2)
-      */
-     public void testWithoutHashMapNullShouldShortCircuitToFalse () {
-         assertFalse(ArraysAndStrings.isUniqueNoHashMap(null));
-     }
+    /**
+     * Question 1 w/o hashmap gives us a O(N^2)
+     */
+    @Test
+    public void testWithoutHashMapNullShouldShortCircuitToFalse() {
+        assertFalse(isUniqueNoHashMap(null));
+    }
 
-     public void testWithoutHashMapEmptyShouldShortCircuitToFalse () {
-         assertFalse(ArraysAndStrings.isUniqueNoHashMap(""));
-     }
+    @Test
+    public void testWithoutHashMapEmptyShouldShortCircuitToFalse() {
+        assertFalse(isUniqueNoHashMap(""));
+    }
 
-     public void testWithoutHashMapStringWithDuplicatesIsNotUnique() {
-         assertFalse(ArraysAndStrings.isUniqueNoHashMap("object oriented"));
-     }
+    @Test
+    public void testWithoutHashMapStringWithDuplicatesIsNotUnique() {
+        assertFalse(isUniqueNoHashMap("object oriented"));
+    }
 
-     public void testWithoutHashMapStringWithoutDuplicatesIsUnique() {
-         assertTrue(ArraysAndStrings.isUniqueNoHashMap("abc"));
-     }
+    @Test
+    public void testWithoutHashMapStringWithoutDuplicatesIsUnique() {
+        assertTrue(isUniqueNoHashMap("abc"));
+    }
 
-     /**
-      * Question 2
-      */
+    /**
+     * Question 2
+     */
+    @Test
     public void testVoidOriginalReturnsFalse() {
-        assertFalse(ArraysAndStrings.isPermutation(null, "string"));
+        assertFalse(isPermutation(null, "string"));
     }
 
+    @Test
     public void testVoidPotentialReturnsFalse() {
-        assertFalse(ArraysAndStrings.isPermutation("string", null));
+        assertFalse(isPermutation("string", null));
     }
 
+    @Test
     public void testEmptyOriginalReturnsFalse() {
-        assertFalse(ArraysAndStrings.isPermutation("", "string"));
+        assertFalse(isPermutation("", "string"));
     }
 
+    @Test
     public void testEmptyPotentialReturnsFalse() {
-        assertFalse(ArraysAndStrings.isPermutation("string", ""));
+        assertFalse(isPermutation("string", ""));
     }
 
+    @Test
     public void testFailureCaseReturnsFalse() {
-        assertFalse(ArraysAndStrings.isPermutation("abc", "d"));
+        assertFalse(isPermutation("abc", "d"));
     }
 
+    @Test
     public void testSuccessCaseReturnsTrue() {
-        assertFalse(ArraysAndStrings.isPermutation("ab", "ba"));
+        assertFalse(isPermutation("ab", "ba"));
     }
+
+    /**
+     * Question 3
+     */
+    @Test
+    public void testConvertToURLNullUrlReturnsNull() {
+        assertNull(convertToURL(null, 10));
+    }
+
+    @Test
+    public void testConvertToURLNullCountReturnsNull() {
+        assertNull(convertToURL("mr bobdobalina", null));
+    }
+
+    @Test
+    public void testConvertToURLSuccessCase() {
+        assertEquals(convertToURL("Mr John Smith     ", 13), "Mr%20John%20Smith");
+    }
+
+    /**
+     * Question 4
+     */
+    @Test
+    public void testIsPalindromeNullYieldsFalse() {
+        assertFalse(isPalindromePermutation(null));
+    }
+
+    @Test
+    public void testIsPalindromeEmptyYieldsEmpty() {
+        assertTrue(isPalindromePermutation(""));
+    }
+
+    @Test
+    public void testIsPalindromeNormal() {
+        assertTrue(isPalindromePermutation("aa"));
+    }
+
+    @Test
+    public void testIsPalindromeNormalWithMidpoint() {
+        assertTrue(isPalindromePermutation("aba"));
+    }
+
+    @Test
+    public void testIsPalindromeNormalWithMidpointJumbled() {
+        assertTrue(isPalindromePermutation("aab"));
+    }
+
+    @Test
+    public void testIsPalindromeNormalCase() {
+        assertTrue(isPalindromePermutation("tact coa"));
+    }
+
+
+    @Test
+    public void testIsPalindromeIgnoreCase() {
+        assertTrue(isPalindromePermutation("TactCoa"));
+    }
+
+    /**
+     * Question 1.5
+     */
+    @Test
+    public void testIsOneEditAwayNullReturnsNull() {
+        assertNull(isOneEditAway(null,"Example"));
+        assertNull(isOneEditAway("Example",null));
+        assertNull(isOneEditAway(null,null));
+    }
+
+    @Test
+    public void testIsOneEditAwayInputsAreSame() {
+        assertTrue(isOneEditAway("same", "same"));
+    }
+
+    @Test
+    public void testIsOneEditAwayInputsOneNeedsInsertion() {
+        assertTrue(isOneEditAway("sme", "same"));
+    }
+
+    @Test
+    public void testIsOneEditAwayInputsOneNeedsDeletion() {
+        assertTrue(isOneEditAway("same", "same"));
+    }
+
+
+    @Test
+    public void testIsOneEditAwayInputsOneNeedsReplacement() {    
+        assertTrue(isOneEditAway("same", "same"));
+    }
+
 }
